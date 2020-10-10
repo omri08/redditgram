757a3bf69ac9cbd7f8a52da97b023e68c53ec819
omri08 <54754198+omri08@users.noreply.github.com>
2020-10-10 13:55:05 +0300

    Added explore icon to Header componenet

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -2,19 +2,22 @@ import React from "react";
 import { Input, Avatar } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import { Link, useLocation } from "react-router-dom";
-import { useComponent } from "../../utils/hooks";
+import { useComponent, useWindowSize } from "../../utils/hooks";
 import { ReactComponent as HomeIcon } from "../../assets/svg/home.svg";
 import { ReactComponent as HomeFilledIcon } from "../../assets/svg/homeFilled.svg";
 import { ReactComponent as HeartIcon } from "../../assets/svg/heart.svg";
 import { ReactComponent as HeartFilledIcon } from "../../assets/svg/heartFilled.svg";
+import { ReactComponent as ExploreIcon } from "../../assets/svg/explore.svg";
 import styles from "./Header.module.scss";
 
 const { Search } = Input;
 
 export default function Header() {
   const { pathname } = useLocation();
-  const Home = useComponent(HomeIcon, HomeFilledIcon, "/", pathname);
-  const Heart = useComponent(HeartIcon, HeartFilledIcon, "/loved", pathname);
+  const Home = useComponent(HomeIcon, HomeFilledIcon, "/" === pathname);
+  const Heart = useComponent(HeartIcon, HeartFilledIcon, "/loved" === pathname);
+
+  const { width } = useWindowSize();
   return (
     <div className={styles.container}>
       <h1>Hello World</h1>
@@ -31,6 +34,13 @@ export default function Header() {
         <Link to="/loved">
           <Heart />
         </Link>
+        {width <= 640 ? (
+          <Link to="/explore">
+            <ExploreIcon />
+          </Link>
+        ) : (
+          ""
+        )}
         <Avatar icon={<UserOutlined />} />
       </span>
     </div>
--- client/src/utils/hooks.js
+++ client/src/utils/hooks.js
@@ -1,12 +1,40 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 
-export function useComponent(base, ChangeTo, expectedValue, value) {
+export function useComponent(base, ChangeTo, bool) {
   const [component, setComponent] = useState(base);
 
   const res = useMemo(() => {
-    if (expectedValue === value) setComponent(ChangeTo);
+    if (bool) setComponent(ChangeTo);
     else setComponent(base);
-  }, [expectedValue, value]);
+  }, [bool]);
 
   return component;
+}
+
+export function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+
+    height: undefined,
+  });
+
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+
+        height: window.innerHeight,
+      });
+    }
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+
+  return windowSize;
 }
