5657e7997706946cecb85972a5e53c7323f459d9
omri08 <54754198+omri08@users.noreply.github.com>
2020-10-19 14:59:58 +0300

    Added size prop to card and deleted position

--- client/src/components/card/Card.js
+++ client/src/components/card/Card.js
@@ -2,12 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import { useHistory } from "react-router-dom";
 import style from "./Card.module.scss";
-function Card({ post }) {
+function Card({ post, size }) {
   const history = useHistory();
+  console.log(size);
   return (
     <div
       className={style.container}
       onClick={() => history.push(`/post/${post.id}`)}
+      style={size}
     >
       {post.type === "VIDEO" ? (
         <video autoPlay loop controls muted>
--- client/src/components/gallery/Gallery.js
+++ client/src/components/gallery/Gallery.js
@@ -4,10 +4,14 @@ import Card from "../card/Card";
 import styles from "./Gallery.module.scss";
 
 export default function Gallery({ posts }) {
+  const cardStyle = {
+    width: "350px",
+    height: "350px",
+  };
   return (
     <div className={styles.container}>
       {posts.posts.map((post) => (
-        <Card key={post.id} post={post} />
+        <Card key={post.id} post={post} size={cardStyle} />
       ))}
     </div>
   );
