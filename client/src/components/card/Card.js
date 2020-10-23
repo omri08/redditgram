df92b72191b5f6dc6965a9dc60a82704cd6db842
omri08 <54754198+omri08@users.noreply.github.com>
2020-10-23 11:06:48 +0300

    Added skeleton and custom style

--- client/src/components/card/Card.js
+++ client/src/components/card/Card.js
@@ -1,29 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { useHistory } from "react-router-dom";
+import { Skeleton } from "antd";
+
 import style from "./Card.module.scss";
-function Card({ post, size }) {
-  const history = useHistory();
-  console.log(size);
+function Card({ post, customStyle, onClick, controls }) {
+  const [loaded, setLoaded] = useState(false);
+  const visible = !loaded ? { display: "none" } : {};
+
+  function showComponent() {
+    return post.type === "VIDEO" ? (
+      <video autoPlay loop controls={controls} muted>
+        <source src={post.media} type="video/mp4"></source>
+      </video>
+    ) : (
+      <img
+        style={visible}
+        onLoad={() => setLoaded(true)}
+        alt={post.title}
+        src={post.media}
+      />
+    );
+  }
   return (
     <div
       className={style.container}
-      onClick={() => history.push(`/post/${post.id}`)}
-      style={size}
+      style={customStyle}
+      onClick={onClick ? () => onClick() : () => {}}
     >
-      {post.type === "VIDEO" ? (
-        <video autoPlay loop controls muted>
-          <source src={post.media} type="video/mp4"></source>
-        </video>
-      ) : (
-        <img alt={post.title} src={post.media} />
+      {!loaded && post.type !== "VIDEO" && (
+        <Skeleton.Avatar active={true} style={customStyle} shape="square" />
       )}
+      {showComponent()}
     </div>
   );
 }
 
 Card.propTypes = {
   post: PropTypes.object.isRequired,
+  customStyle: PropTypes.object.isRequired,
+  controls: PropTypes.bool.isRequired,
 };
 
 export default React.memo(Card);
--- client/src/components/gallery/Gallery.js
+++ client/src/components/gallery/Gallery.js
@@ -1,17 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useHistory } from "react-router-dom";
 import Card from "../card/Card";
 import styles from "./Gallery.module.scss";
 
 export default function Gallery({ posts }) {
-  const cardStyle = {
-    width: "350px",
-    height: "350px",
-  };
+  const history = useHistory();
+  const cardStyle = React.useCallback(
+    {
+      width: "350px",
+      height: "350px",
+      cursor: "pointer",
+    },
+    []
+  );
   return (
     <div className={styles.container}>
       {posts.posts.map((post) => (
-        <Card key={post.id} post={post} size={cardStyle} />
+        <Card
+          key={post.id}
+          post={post}
+          customStyle={cardStyle}
+          onClick={() => history.push(`/post/${post.id}`)}
+          controls={false}
+        />
       ))}
     </div>
   );
